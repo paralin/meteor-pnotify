c5f5ece6e7708e70af7f460f11226b74789bf92f
Christian Stewart <kidovate@gmail.com>
2014-04-18 19:34:37 -0700

    Added a compatability layers

--- package.js
+++ package.js
@@ -6,6 +6,7 @@ Package.on_use(function (api) {
     api.use('jquery', 'client');
     api.add_files([
         'lib/js/jquery.pnotify.js',
+        'lib/js/bridge.js',
         'lib/css/jquery.pnotify.default.css'
     ],'client');
 });
